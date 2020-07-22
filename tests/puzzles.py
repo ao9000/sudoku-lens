e2d95364695c6bdf13455635ff370e8a31d47b14
adrian <ongadrian98@gmail.com>
2020-07-22 17:29:33 +0800

    Added "not fun" puzzles and tests

--- tests/puzzles.py
+++ tests/puzzles.py
@@ -149,3 +149,35 @@ def difficult_puzzles():
     difficult_puzzles_solutions = [difficult1_solution, difficult2_solution]
 
     return difficult_puzzles, difficult_puzzles_solutions
+
+
+@pytest.fixture
+def not_fun_puzzles():
+    not_fun1 = np.array([
+        [BLANK_STATE, 2, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE],
+        [BLANK_STATE, BLANK_STATE, BLANK_STATE, 6, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, 3],
+        [BLANK_STATE, 7, 4, BLANK_STATE, 8, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE],
+        [BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, 3, BLANK_STATE, BLANK_STATE, 2],
+        [BLANK_STATE, 8, BLANK_STATE, BLANK_STATE, 4, BLANK_STATE, BLANK_STATE, 1, BLANK_STATE],
+        [6, BLANK_STATE, BLANK_STATE, 5, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE],
+        [BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, 1, BLANK_STATE, 7, 8, BLANK_STATE],
+        [5, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, 9, BLANK_STATE, BLANK_STATE, BLANK_STATE],
+        [BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, BLANK_STATE, 4, BLANK_STATE]
+    ])
+
+    not_fun1_solution = np.array([
+        [1, 2, 6, 4, 3, 7, 9, 5, 8],
+        [8, 9, 5, 6, 2, 1, 4, 7, 3],
+        [3, 7, 4, 9, 8, 5, 1, 2, 6],
+        [4, 5, 7, 1, 9, 3, 8, 6, 2],
+        [9, 8, 3, 2, 4, 6, 5, 1, 7],
+        [6, 1, 2, 5, 7, 8, 3, 9, 4],
+        [2, 6, 9, 3, 1, 4, 7, 8, 5],
+        [5, 4, 8, 7, 6, 9, 2, 3, 1],
+        [7, 3, 1, 8, 5, 2, 6, 4, 9]
+    ])
+
+    not_fun_puzzles = [not_fun1]
+    not_fun_puzzles_solutions = [not_fun1_solution]
+
+    return not_fun_puzzles, not_fun_puzzles_solutions
--- tests/test_backtracking.py
+++ tests/test_backtracking.py
@@ -1,5 +1,5 @@
 from backtracking import backtracking
-from tests.puzzles import easy_puzzles, intermediate_puzzles, difficult_puzzles
+from tests.puzzles import easy_puzzles, intermediate_puzzles, difficult_puzzles, not_fun_puzzles
 
 
 def test_easy_puzzles(easy_puzzles):
@@ -27,6 +27,17 @@ def test_intermediate_puzzles(intermediate_puzzles):
 def test_difficult_puzzles(difficult_puzzles):
     # Unpack
     puzzles, solutions = difficult_puzzles
+
+    for puzzle, solution in zip(puzzles, solutions):
+        solved, step = backtracking(puzzle)
+
+        assert (solved == solution).all()
+        assert step != 0
+
+
+def test_not_fun_puzzles(not_fun_puzzles):
+    # Unpack
+    puzzles, solutions = not_fun_puzzles
 
     for puzzle, solution in zip(puzzles, solutions):
         solved, step = backtracking(puzzle)
