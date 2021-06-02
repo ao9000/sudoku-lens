bac8f09ee55686ab44d5fb460c4d08ced4ebefa9
adrian <ongadrian98@gmail.com>
2021-06-02 17:27:09 +0800

    Improved backtracking code

--- backtracking.py
+++ backtracking.py
@@ -1,95 +1,81 @@
 import numpy as np
-import math
 
 BLANK_STATE = 0
 
 
-def backtracking(board, index=(0, 0), step=1):
-    # Unpack
-    row_index, column_index = index
+def backtracking(board):
+    index = get_next_index(board)
 
-    # Check if passed the last box
-    if is_board_full(board):
-        return board, step-1
+    # Check if index was found
+    if index:
+        # Unpack index
+        row_index, column_index = index
 
-    # Check if cursor is at a blank state
-    if board[row_index, column_index] == BLANK_STATE:
-        # Blank state
-        # Assign number to box
-        for num in range(1, 11):
-            if num == 10:
-                # Every possible number not valid
-                # Perform backtrack
-                board[row_index, column_index] = BLANK_STATE
-                return board, step - 1
+        # Try 1-9
+        for num in range(1, 10):
+            # Check if current loop number is valid for the cell
+            if check_valid(board, (row_index, column_index), num):
+                # Valid, assign number to cell
+                board[row_index][column_index] = num
 
-            else:
-                # Assign
-                board[row_index, column_index] = num
+                # Move on to next available cell
+                board = backtracking(board)
 
-                # Check if valid
-                if check_valid(board, (row_index, column_index)):
-                    # Valid, move to next box
-                    board, step = backtracking(board, get_next_index(index), step+1)
+                # Check if puzzle is solved
+                if not get_next_index(board):
+                    return board
 
-                    # Break loop if puzzle is solved
-                    if is_board_full(board):
-                        break
-
-                else:
-                    # Not valid but there is other possible values not tested
-                    continue
-
+        board[row_index][column_index] = 0
+        return board
     else:
-        # Not blank state, default values
-        # Shifting to next box
-        board, step = backtracking(board, get_next_index(index), step)
-
-    return board, step
+        # No empty cell, meaning puzzle has been completed
+        return board
 
 
-def check_valid(board, index):
+def get_next_index(board):
+    for row_index in range(0, 9):
+        for column_index in range(0, 9):
+            if board[row_index][column_index] == BLANK_STATE:
+                return row_index, column_index
+
+    return None
+
+
+def check_valid(board, index, cell_value):
+    # Unpack index
     row_index, column_index = index
 
     # Check if number is valid
     # Check row
-    row = np.delete(board[row_index], obj=column_index)
-    if board[row_index, column_index] in row:
+    if cell_value in board[row_index]:
         return False
 
     # Check column
-    column = np.delete(board[:, column_index], obj=row_index)
-    if board[row_index, column_index] in column:
+    if cell_value in list(zip(*board))[column_index]:
         return False
 
-    # Check 3x3 box area
-    box_row_index = (math.ceil((row_index + 1) / 3) - 1) * 3
-    box_column_index = (math.ceil((column_index + 1) / 3) - 1) * 3
-
-    box_row_index_relative = row_index % 3
-    box_column_index_relative = column_index % 3
-
-    box = np.delete(np.ravel(board[box_row_index:box_row_index + 3, box_column_index:box_column_index + 3], order='C'),
-                    obj=box_row_index_relative * 3 + box_column_index_relative)
-    if board[row_index, column_index] in box:
+    # Check 3x3 box matrix
+    affected_row = board[(row_index//3)*3: (row_index//3)*3+3]
+    matrix = list(zip(*affected_row))[(column_index//3)*3: (column_index//3)*3+3]
+    if any(cell_value in row for row in matrix):
         return False
 
     return True
 
 
-def is_board_full(board):
-    if BLANK_STATE in board:
-        return False
+board = [
+[1, 2, 3, 4, 0, 0, 5, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[6, 0, 0, 0, 7, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 3, 0, 0],
+[0, 0, 0, 0, 8, 0, 0, 0, 0],
+[8, 0, 0, 0, 0, 0, 0, 0, 7],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 5, 0, 0, 0, 0, 0]
+]
 
-    return True
 
-
-def get_next_index(index):
-    row_index, column_index = index
-
-    if column_index == 8:
-        # Next row
-        return row_index + 1, 0
-    else:
-        # Next column
-        return row_index, column_index + 1
+print(backtracking(board))
+print(board)
+print("DONE")
