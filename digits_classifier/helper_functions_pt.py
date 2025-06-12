2bfab25f7dca993fac4e065c44916e290cb7c67c
Adrian Ong <ongadrian98@gmail.com>
2025-06-12 22:05:46 +0800

    Added a new model definition

--- digits_classifier/helper_functions_pt.py
+++ digits_classifier/helper_functions_pt.py
@@ -42,27 +42,58 @@ def get_mnist_dataset_loader(save_path, train, transform, batch_size):
         pin_memory=True
     )
 
+# Version 1
+# # Model definition
+# class MNISTClassifier(nn.Module):
+#     """
+#     Source: https://nextjournal.com/gkoehler/pytorch-mnist
+#     """
+#     def __init__(self):
+#         super(MNISTClassifier, self).__init__()
+#         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+#         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+#         self.conv2_drop = nn.Dropout2d()
+#         self.fc1 = nn.Linear(320, 50)
+#         self.fc2 = nn.Linear(50, 9) # Change from 10 classes (0-9) to 9 classes (1-9)
+#
+#     def forward(self, x):
+#         x = F.relu(F.max_pool2d(self.conv1(x), 2))
+#         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+#         x = x.view(-1, 320)
+#         x = F.relu(self.fc1(x))
+#         x = F.dropout(x, training=self.training)
+#         x = self.fc2(x)
+#         return F.log_softmax(x)
+
 # Model definition
 class MNISTClassifier(nn.Module):
     """
-    Source: https://nextjournal.com/gkoehler/pytorch-mnist
+    v1 Source: https://nextjournal.com/gkoehler/pytorch-mnist
+    v2 Source: https://github.com/PyTorch/examples/blob/main/mnist/main.py
     """
     def __init__(self):
         super(MNISTClassifier, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 9) # Change from 10 classes (0-9) to 9 classes (1-9)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 9)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
         x = self.fc2(x)
-        return F.log_softmax(x)
+        output = F.log_softmax(x, dim=1)
+        return output
 
 
 def build_model(optimizer_name, **kwargs):
